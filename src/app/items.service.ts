b2ae732dbc477630551746065feeffbcf9a205cf
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-07-02 00:30:44 -0700

    Fixed production environment, one final bug with map.

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+//import 'rxjs/Rx';
 // import { AngularFireDatabase } from '@angular/fire/database';
 // import { Observable } from 'rxjs';
 
